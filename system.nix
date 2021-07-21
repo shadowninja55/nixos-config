3f3eb43557fa09e644011ddfa83722e24ad19f43
shadowninja55 <shadowninja1050@gmail.com>
2021-07-21 07:57:17 -0400

    specify path to grub theme.txt

--- system.nix
+++ system.nix
@@ -15,7 +15,7 @@
       repo = "grub";
       rev = "ad5b6bd4b159fea4950918a5510864ebb551519d";
       sha256 = "12c76qs6p0fpbn4c3i0va5ibr711fgdhhh4ci86hmd5ss93p4b5y";
-    } + "/dracula";
+    } + "/dracula/theme.txt";
     splashImage = null;
     gfxmodeBios = "1920x1080";
   };
