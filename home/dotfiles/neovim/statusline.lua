c383ff3d92178b7d7134cb1e3540eaad50b9a254
shadowninja55 <shadowninja1050@gmail.com>
2021-09-12 10:55:38 -0400

    redo feline setup

--- home/dotfiles/neovim/statusline.lua
+++ home/dotfiles/neovim/statusline.lua
@@ -1,16 +1,6 @@
 local components = {
-  left = {
-    active = {},
-    inactive = {}
-  },
-  mid = {
-    active = {},
-    inactive = {}
-  },
-  right = {
-    active = {},
-    inactive = {}
-  }
+  active = {{}, {}, {}},
+  inactive = {{}, {}}
 }
 
 local colors = {
@@ -42,7 +32,7 @@ local vi_mode_text = {
 
 local vi_mode_utils = require('feline.providers.vi_mode')
 
-components.left.active[1] = {
+table.insert(components.active[1], {
   provider = function()
     local mode = vi_mode_text[vim.fn.mode()]
     mode = mode and mode or 'UNKNOWN'
@@ -57,9 +47,9 @@ components.left.active[1] = {
   }
   end,
   right_sep = 'slant_right',
-}
+})
 
-components.left.active[2] = {
+table.insert(components.active[1], {
   provider = 'file_info',
   hl = {
     fg = colors.fg,
@@ -71,10 +61,10 @@ components.left.active[2] = {
     {str = ' ', hl = {bg = colors.orange}}
   },
   right_sep = 'slant_right'
-}
+})
 
 
-components.right.active[1] = {
+table.insert(components.active[3], {
   provider = 'line_percentage',
   hl = {
     fg = colors.fg,
@@ -89,9 +79,9 @@ components.right.active[1] = {
     {str = ' ', hl = {bg = colors.yellow}},
     'slant_right_2'
   }
-}
+})
 
-components.right.active[2] = {
+table.insert(components.active[3], {
   provider = 'position',
   hl = {
     fg = colors.fg,
@@ -103,12 +93,11 @@ components.right.active[2] = {
     {str = ' ', hl = {bg = colors.green}}
   },
   right_sep = {str = ' ', hl = {bg = colors.green}}
-}
+})
 
 require('feline').setup {
   default_bg = colors.bg,
   default_fg = colors.fg,
   colors = colors,
-  vi_mode_colors = vi_mode_colors,
   components = components
 }
